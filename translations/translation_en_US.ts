da99f3a63007d186b978ee46daf3e574a091737b
kyklospi <rachmawati.atika@gmail.com>
2023-07-11 20:22:35 +0200

    Add files via upload

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,294 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>Aufmerksamkeit/behavior.xar:/Begruessung</name>
+        <message>
+            <source>Hallo!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hallo!</translation>
+        </message>
+    </context>
+    <context>
+        <name>Aufmerksamkeit/behavior.xar:/Begrüßung</name>
+        <message>
+            <location filename="Aufmerksamkeit/behavior.xar" line="0"/>
+            <source>Hallo!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hallo!</translation>
+        </message>
+    </context>
+    <context>
+        <name>Aufmerksamkeit/behavior.xar:/Bereit?</name>
+        <message>
+            <source>Seid ihr bereit</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Seid ihr bereit</translation>
+        </message>
+        <message>
+            <source>Seid ihr bereit?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Seid ihr bereit?</translation>
+        </message>
+    </context>
+    <context>
+        <name>Aufmerksamkeit/behavior.xar:/PMR Begeisterung</name>
+        <message>
+            <source>Ihr werdet sehen,  wie leicht die Übungen sind. Am Ende der Sitzung werdet ihr die Übungen selbst nachmachen können.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ihr werdet sehen,  wie leicht die Übungen sind. Am Ende der Sitzung werdet ihr die Übungen selbst nachmachen können.</translation>
+        </message>
+        <message>
+            <source>Am Ende der Sitzung werdet ihr die Übungen selbst nachmachen können.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Am Ende der Sitzung werdet ihr die Übungen selbst nachmachen können.</translation>
+        </message>
+    </context>
+    <context>
+        <name>Aufmerksamkeit/behavior.xar:/PMR Begeisterung 1</name>
+        <message>
+            <location filename="Aufmerksamkeit/behavior.xar" line="0"/>
+            <source>Ihr werdet sehen,  wie leicht die Übungen sind. </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ihr werdet sehen,  wie leicht die Übungen sind. </translation>
+        </message>
+        <message>
+            <source>Ihr werdet sehen,  wie leicht die Uebungen sind. </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ihr werdet sehen,  wie leicht die Uebungen sind. </translation>
+        </message>
+    </context>
+    <context>
+        <name>Aufmerksamkeit/behavior.xar:/PMR Begeisterung 2</name>
+        <message>
+            <location filename="Aufmerksamkeit/behavior.xar" line="0"/>
+            <source>Am Ende der Sitzung werdet ihr die Übungen selbst nachmachen können.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Am Ende der Sitzung werdet ihr die Übungen selbst nachmachen können.</translation>
+        </message>
+        <message>
+            <source>Am Ende der Sitzung werdet ihr die Uebungen selbst nachmachen koennen.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Am Ende der Sitzung werdet ihr die Uebungen selbst nachmachen koennen.</translation>
+        </message>
+    </context>
+    <context>
+        <name>Aufmerksamkeit/behavior.xar:/PMR Info</name>
+        <message>
+            <location filename="Aufmerksamkeit/behavior.xar" line="0"/>
+            <source>Die Progressive-Muskelentspannung Übungen hilft euch zu entspannen.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Die Progressive-Muskelentspannung Übungen hilft euch zu entspannen.</translation>
+        </message>
+        <message>
+            <source>Die Progressive-Muskelentspannung Uebungen hilft euch zu entspannen.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Die Progressive-Muskelentspannung Uebungen hilft euch zu entspannen.</translation>
+        </message>
+    </context>
+    <context>
+        <name>Aufmerksamkeit/behavior.xar:/PMR Info 1</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello</translation>
+        </message>
+        <message>
+            <source>Ihr werdet sehen,  wie leicht die Übungen sind. </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ihr werdet sehen,  wie leicht die Übungen sind. </translation>
+        </message>
+    </context>
+    <context>
+        <name>Aufmerksamkeit/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>Aufmerksamkeit/behavior.xar:/Willkommen</name>
+        <message>
+            <location filename="Aufmerksamkeit/behavior.xar" line="0"/>
+            <source>Herzlich Willkommen bei Muskelentspannungsübungen</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Herzlich Willkommen bei Muskelentspannungsübungen</translation>
+        </message>
+        <message>
+            <source>Herzlich Willkommen bei Muskelentspannungsuebungen</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Herzlich Willkommen bei Muskelentspannungsuebungen</translation>
+        </message>
+    </context>
+    <context>
+        <name>Relevanz/behavior.xar:/Bereit?</name>
+        <message>
+            <location filename="Relevanz/behavior.xar" line="0"/>
+            <source>Seid ihr bereit?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Seid ihr bereit?</translation>
+        </message>
+    </context>
+    <context>
+        <name>Relevanz/behavior.xar:/PMR Info</name>
+        <message>
+            <location filename="Relevanz/behavior.xar" line="0"/>
+            <source>Jetzt lernen wir, wie man sich mit Muskelübungen entspannen kann</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Jetzt lernen wir, wie man sich mit Muskelübungen entspannen kann</translation>
+        </message>
+    </context>
+    <context>
+        <name>Relevanz/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>Relevanz/behavior.xar:/Stress-Symptome</name>
+        <message>
+            <source>wenn ihr euch nervös oder ängstlich fühlt, sind dies Beispiele von Stress</source>
+            <comment>Text</comment>
+            <translation type="obsolete">wenn ihr euch nervös oder ängstlich fühlt, sind dies Beispiele von Stress</translation>
+        </message>
+        <message>
+            <location filename="Relevanz/behavior.xar" line="0"/>
+            <source>Wenn ihr euch nervös oder ängstlich fühlt, sind dies Beispiele von Stress</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Wenn ihr euch nervös oder ängstlich fühlt, sind dies Beispiele von Stress</translation>
+        </message>
+    </context>
+    <context>
+        <name>Relevanz/behavior.xar:/Stress-Ursache</name>
+        <message>
+            <location filename="Relevanz/behavior.xar" line="0"/>
+            <source>Im Berufsumfeld kann Stress durch Arbeitsdruck oder Überstunden verursacht werden</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Im Berufsumfeld kann Stress durch Arbeitsdruck oder Überstunden verursacht werden</translation>
+        </message>
+    </context>
+    <context>
+        <name>Zuversicht/behavior.xar:/Arme-Übung</name>
+        <message>
+            <source>Beweg ihr euch eure Ärme</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Beweg ihr euch eure Ärme</translation>
+        </message>
+        <message>
+            <location filename="Zuversicht/behavior.xar" line="0"/>
+            <source>Beweg ihr jetzt eure Ärme!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Beweg ihr jetzt eure Ärme!</translation>
+        </message>
+    </context>
+    <context>
+        <name>Zuversicht/behavior.xar:/Arme-Übung (1)</name>
+        <message>
+            <location filename="Zuversicht/behavior.xar" line="0"/>
+            <source>Beweg ihr jetzt eure Ärme!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Beweg ihr jetzt eure Ärme!</translation>
+        </message>
+    </context>
+    <context>
+        <name>Zuversicht/behavior.xar:/Arme-Übung 2</name>
+        <message>
+            <location filename="Zuversicht/behavior.xar" line="0"/>
+            <source>Nochmal die Ärme auf die andere Richtung</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Nochmal die Ärme auf die andere Richtung</translation>
+        </message>
+    </context>
+    <context>
+        <name>Zuversicht/behavior.xar:/Arme-Übung 2 (1)</name>
+        <message>
+            <location filename="Zuversicht/behavior.xar" line="0"/>
+            <source>Nochmal die Ärme auf die andere Richtung</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Nochmal die Ärme auf die andere Richtung</translation>
+        </message>
+    </context>
+    <context>
+        <name>Zuversicht/behavior.xar:/Arme-Übung Frage</name>
+        <message>
+            <source>könnt ihr der Bewegung folgen?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">könnt ihr der Bewegung folgen?</translation>
+        </message>
+        <message>
+            <location filename="Zuversicht/behavior.xar" line="0"/>
+            <source>könnt ihr der Bewegung folgen? Sag bitte ja oder nein</source>
+            <comment>Text</comment>
+            <translation type="unfinished">könnt ihr der Bewegung folgen? Sag bitte ja oder nein</translation>
+        </message>
+    </context>
+    <context>
+        <name>Zuversicht/behavior.xar:/Arme-Übung Frage (1)</name>
+        <message>
+            <location filename="Zuversicht/behavior.xar" line="0"/>
+            <source>könnt ihr der Bewegung folgen? Sag bitte ja oder nein</source>
+            <comment>Text</comment>
+            <translation type="unfinished">könnt ihr der Bewegung folgen? Sag bitte ja oder nein</translation>
+        </message>
+    </context>
+    <context>
+        <name>Zuversicht/behavior.xar:/Nein Antwort</name>
+        <message>
+            <location filename="Zuversicht/behavior.xar" line="0"/>
+            <source>Das macht nichts,. Wir können sie noch einmal wiederholen</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Das macht nichts,. Wir können sie noch einmal wiederholen</translation>
+        </message>
+    </context>
+    <context>
+        <name>Zuversicht/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Begrüßung</name>
+        <message>
+            <source>Hallo!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hallo!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Bereit?</name>
+        <message>
+            <source>Seid ihr bereit</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Seid ihr bereit</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Willkommen</name>
+        <message>
+            <source>Herzlich Willkommen bei Muskelentspannungsübungen</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Herzlich Willkommen bei Muskelentspannungsübungen</translation>
+        </message>
+    </context>
+</TS>
